c29332fad0c60b1a6d7958a60a437e3e56f4c1ee
Mooni Don <mooncode610@gmail.com>
2016-12-21 13:11:24 -0200

    update products controller

--- client/src/app/auth/services.js
+++
@@ -1,7 +0,0 @@
-// plugins is a alias. see client/build/webpack.base.conf.js
-// import http client
-import { http } from 'plugins/http'
-import { getData } from 'utils/get'
-
-/* eslint-disable import/prefer-default-export */
-export const loadUserData = () => http.get('/me').then(getData)
--- client/src/app/auth/vuex/plugin.js
+++ client/src/app/auth/vuex/plugin.js
@@ -1,15 +1,22 @@
-import localforage from 'localforage'
-import { userTokenStorageKey } from 'src/config'
+import { localStorageSetItem } from 'utils/local'
 import * as TYPES from './mutations-types'
 
 const subscribe = (store) => {
   store.subscribe((mutation, { Auth }) => {
+    if (TYPES.SET_USER === mutation.type) {
+      /**
+       * Sets the user to the local storage
+       * for auto-login purpose
+       */
+      localStorageSetItem('user', { user: Auth.user })
+    }
+
     if (TYPES.SET_TOKEN === mutation.type) {
       /**
        * Sets the token to the local storage
        * for auto-login purpose
        */
-      localforage.setItem(userTokenStorageKey, Auth.token)
+      localStorageSetItem('token', { token: Auth.token })
     }
   })
 }
